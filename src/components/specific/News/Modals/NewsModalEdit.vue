f20d61dc350704d592e878944955481a45aa485f
Anastasia Agarkova <a.agarkova@voskhod.ru>
2023-02-15 11:52:11 +0300

    Исправила исправление рубрик в новости

--- src/components/specific/News/Modals/NewsModalEdit.vue
+++ src/components/specific/News/Modals/NewsModalEdit.vue
@@ -104,7 +104,7 @@ export default {
                 return this.articleData.rubrics ?? '';
             },
             set: function (newVal) {
-                this.articleData.rubrics = newVal;
+                this.articleData.rubrics_id = newVal;
             },
         },
     },
